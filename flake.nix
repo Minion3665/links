7f0919bd402367f5d4ea63eaed85a9cf8c3c231d
Skyler Grey <skyler3665@gmail.com>
2023-08-06 19:21:34 +0000

    wip: update to remove our nixpkgs dependency

--- flake.nix
+++ flake.nix
@@ -1,21 +1,46 @@
 {
   description = "A flake to print out my socials when run";
 
-  outputs = { self, nixpkgs }: let
-    lib = nixpkgs.lib;
-    forAllSystems = lib.genAttrs lib.systems.flakeExposed;
-    echoScript = path: lib.pipe path [
+  outputs = { self }: let
+    pipe = x: fs: builtins.foldl' (val: f: f val) x fs;
+    genAttrs = xs: f: pipe xs [
+      (map (name: { inherit name; value = f name; }))
+      builtins.listToAttrs
+    ];
+    forAllSystems = genAttrs [
+      # Tier 1
+      "x86_64-linux"
+      # Tier 2
+      "aarch64-linux"
+      "x86_64-darwin"
+      # Tier 3
+      "armv6l-linux"
+      "armv7l-linux"
+      "i686-linux"
+      "mipsel-linux"
+
+      # Other platforms with sufficient support in stdenv which is not formally
+      # mandated by their platform tier.
+      "aarch64-darwin"
+      "armv5tel-linux"
+      "powerpc64le-linux"
+      "riscv64-linux"
+    ];  # List is from https://github.com/NixOS/nixpkgs/blob/master/lib/systems/flake-systems.nix
+    echoScript = path: pipe path [
       builtins.readFile
-      (lib.replaceStrings ["\\" "\""] ["\\\\" "\\\""])
+      (builtins.replaceStrings ["\n" "\\" "\""] ["\\n" "\\\\" "\\\""])
       (textToEcho: ''
-        echo "${textToEcho}"
+        echo -e "${textToEcho}"
       '')
     ];
   in {
     packages = forAllSystems (system: {
-    	links = nixpkgs.legacyPackages.${system}.writeShellScriptBin
-	  "links"
-	  (echoScript ./links.txt);
+    	links = derivation {
+	  inherit system;
+	  name = "links";
+	  builder = "/bin/sh";
+	  args = [./builder.sh (echoScript ./links.txt)];
+	};
         default = self.packages.${system}.links;
     });
   };
